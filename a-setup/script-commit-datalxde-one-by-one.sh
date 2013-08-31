fe2f00cc49bd6f0db7c8ac7fb2609ec9139a9cc9
minghua <minghua@localhost>
2013-08-30 19:28:14 -0700

    lxde data commit script retry push

--- a-setup/script-commit-datalxde-one-by-one.sh
+++ a-setup/script-commit-datalxde-one-by-one.sh
@@ -19,22 +19,27 @@ for FILENAME in $flist ; do
       git add $FILENAME $FILENAME-sha1
       git commit -m "file $FILENAME $FILENAME-sha1"
 
+    while true ; do
+      issync=`git status | grep ahead`
+     if [[ "$issync" =~ "ahead" ]]; then
     echo
+    echo -n "  sleep 10  "; sleep 10
     echo "  Remote push: "
     echo
       git push origin master
 
       if [ $? -ne 0 ]; then
-          echo "ERROR: no file $FILENAME"
-          edata=`date`
-          echo "  start: $sdata"
-          echo "  done:  $edata"
-          exit 1
+          echo "ERROR: push failed file $FILENAME"
+          echo "  retry ...."
       fi
+     else
+       break
+     fi
+    done
 
     echo
       issync=`git status | grep ahead`
-      if [ "$issync" == "ahead" ]; then
+      if [[ "$issync" =~ "ahead" ]]; then
           echo "ERROR: local ahead for file $FILENAME"
           edata=`date`
           echo "  start: $sdata"
